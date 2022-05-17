70aa31344ffb4560092c319d97377b9b08c7eb5a
BebnevRA <BebnevRA@gmail.com>
2022-05-17 15:17:21 +0300

    del pycache

--- test.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import telebot
-
-if __name__ == '__main__':
-    # bot = telebot.TeleBot('5295909082:AAGv4Ac6987pH5xXjCeSEm_wEeYtFud12yM')
-    # bot.send_message('297452818', 'rere')
-    time.sleep(10)
-    print('1')
